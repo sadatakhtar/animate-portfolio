9647772acdd50e05ae64b373a9d0c9a80e2e8438
sadatakhtar <sadat0akhtar@gmail.com>
2021-03-01 18:20:43 +0000

    removed unused files

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
-import Header from './components/headerComponent/Header'
-import { BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom'
+import { BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 import HomePage from './pages/homePage/HomePage';
 
 function App() {
